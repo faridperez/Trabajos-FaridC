08890a9fce2c0a0ef4097c1ac8e2fc4fda971a87
faridperez <131931890+faridperez@users.noreply.github.com>
2023-05-18 12:56:54 -0600

    Update bubblesort.c

--- bubblesort.c
+++ bubblesort.c
@@ -4,15 +4,18 @@ int main() {
     int arr[10] = {5, 2, 9, 1, 7, 6, 4, 3, 8, 10};
     int i, j, temp;
 
-    printf("Arreglo original: ");
-    for (i = 0; i < 10; i++) {
+    printf("orden original: ");
+    for (i = 0; i < 10; i++)
+    {
         printf("%d ", arr[i]);
     }
 
-    for (i = 0; i < 10 - 1; i++) {
-        for (j = 0; j < 10 - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                // Intercambiar arr[j] y arr[j+1]
+    for (i = 0; i < 10 - 1; i++)
+    {
+        for (j = 0; j < 10 - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
                 temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
@@ -20,8 +23,9 @@ int main() {
         }
     }
 
-    printf("\nArreglo ordenado: ");
-    for (i = 0; i < 10; i++) {
+    printf("\norden menor a mayor: ");
+    for (i = 0; i < 10; i++)
+    {
         printf("%d ", arr[i]);
     }
 
