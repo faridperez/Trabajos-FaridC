f93fd731a6e809c8e70b3625e310c84ff4679428
faridperez <131931890+faridperez@users.noreply.github.com>
2023-05-22 08:57:47 -0600

    Update numeroazarv1.c
    version 2 de numero azar

--- numeroazarv1.c
+++ numeroazarv1.c
@@ -4,7 +4,7 @@ int main()
 {
     int intento, numerosecreto;
     numerosecreto=6;
-    printf("ADIVINE EL NUMERO SRECRETO \n");
+    printf("ADIVINE EL NUMERO SECRETO \n");
     scanf("%i", &intento);
     if(intento==numerosecreto)
     {
@@ -12,7 +12,14 @@ int main()
     }
     else
     {
-        printf("incorrecto");
+        if(intento>numerosecreto)
+        {
+            printf("el numero es mayor que el #secreto");
+        }
+        else
+        {
+            printf("el numero es menor que el #secreto");
+        }
     }
     return 0;
 }
