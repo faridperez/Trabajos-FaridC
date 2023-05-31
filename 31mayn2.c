f57e02abd8cf350df98f92410f4f52d7ea66b1f0
faridperez <131931890+faridperez@users.noreply.github.com>
2023-05-31 07:54:30 -0600

    Add files via upload

---
+++ 31mayn2.c
@@ -0,0 +1,25 @@
+/*farid yael perez de gabriel
+31 de mayo de 2023
+programa n2*/
+
+#include <stdio.h>
+int
+main ()
+{
+    int a,b,c;
+    int codigo, farenheit,coord_x,coord_y;
+    
+    codigo=3467;
+    farenheit=123.456;
+    coord_x=525;
+    coord_y=725;
+    a=b=c=45;
+    a=(b=(c=45));
+    printf ("%i valor de la variable 'codigo'\n",codigo);
+    printf ("%i valor de la variable 'farenheit'\n", farenheit);
+    printf ("%i valor de la variable 'coord_x'\n",coord_x);
+    printf ("%i valor de la variable 'coord_y'\n",coord_y);
+    printf ("%i valor de la variable 'a'\n",a);
+    printf ("%i valor de la variable 'b'\n",b);
+    printf ("%i valor de la variable 'c'\n",c);
+}
