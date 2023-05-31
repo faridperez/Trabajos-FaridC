7a578ae74b9fc055dc90b91b9a06f8c9c193912d
faridperez <131931890+faridperez@users.noreply.github.com>
2023-05-31 07:43:56 -0600

    Add files via upload

---
+++ 31mayn1.c
@@ -0,0 +1,16 @@
+/*farid yael perez de gabriel
+31 de mayo de 2023
+programa n1*/
+
+
+#include <stdio.h>
+int
+main ()
+{
+  int num;
+  printf ("el tamaño de la variable de coma flotante es %ld", sizeof (float));
+  printf (" bytes\n");
+  printf ("el tamaño de la variable de doble presicion es %ld", sizeof (double));
+  printf (" bytes\n");
+  return 0;
+}
