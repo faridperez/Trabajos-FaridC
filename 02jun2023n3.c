72890449c4f25664980300845c0e08bf041c04ee
faridperez <131931890+faridperez@users.noreply.github.com>
2023-06-02 09:44:02 -0600

    Add files via upload

---
+++ 02jun2023n3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/*divisible o nop con 'if-else'
+
+
+farid yael perez de gabriel
+02/jun/2023 numero 3*/
+
+
+int main()
+{
+    int dividendo, divisor, residuo, cociente;
+    printf("escriba el primer numero: \n");
+    scanf("%d", &dividendo);
+    printf("escriba el segundo numero: \n");
+    scanf("%d", &divisor);
+    residuo=dividendo%divisor; cociente=dividendo/divisor;
+    if(residuo!=0){
+        printf("el numero %d NO es divisible entre %d\n", dividendo, divisor);
+    } else{
+        printf("el numero %d SI es divisible entre %d\n\n", dividendo, divisor);
+        printf("dividendo: %d\ndivisor: %d\ncociente: : %d\nresiduo : %d\n", dividendo, divisor, cociente, residuo);
+        printf("%d / %d = %i", dividendo,divisor, cociente);
+    }
+    return 0;
+}
