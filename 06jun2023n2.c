4b286e88ee4baaee9109de66cadb1f04d5060836
faridperez <131931890+faridperez@users.noreply.github.com>
2023-06-06 09:44:15 -0600

    Add files via upload

---
+++ 06jun2023n2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/*estructura 'while'
+
+02/jun/2023 act2
+
+farid yael perez de gabriel*/
+int main()
+{
+    int contador=1;
+    while(contador<100)
+    {
+    printf("%d \n", contador);
+    contador--;
+    }
+    return 0;
+}
+
