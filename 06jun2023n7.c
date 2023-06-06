3869b59e3c029787546c5ebf91983ff7d96c483a
faridperez <131931890+faridperez@users.noreply.github.com>
2023-06-06 10:44:12 -0600

    Add files via upload

---
+++ 06jun2023n7.c
@@ -0,0 +1,19 @@
+#include <stdio.h> 
+/*
+Farid Yael Pérez De Gabriel
+*/
+ int main() 
+ { 
+    int contador=1;
+    while(contador<25)
+    {
+        printf("%d\n", contador);
+        contador++;
+    }
+    /*
+    sin las llaves solo ejecutara en bucle la
+    instrucción de escribir el número
+    con las llaves incluye el contador
+    */
+    return 0; 
+ }
