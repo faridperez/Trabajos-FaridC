17af6b3bb91f810ad3e3a7f90ea36b580a67eac4
faridperez <131931890+faridperez@users.noreply.github.com>
2023-06-06 09:59:29 -0600

    Add files via upload

---
+++ 06jun2023n4.c
@@ -0,0 +1,12 @@
+#include <stdio.h> 
+  
+ int main() 
+ { 
+    int contador=1;
+    while(contador>100) //si la prueba lógica es falsa, no se ejecuta la sentencia de instrucciones
+    {
+        printf("%d\n", contador);
+        contador++;
+    }
+    return 0; 
+ }
