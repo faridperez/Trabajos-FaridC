146b0c31d601c101516d975b8af96da30222e9c1
faridperez <131931890+faridperez@users.noreply.github.com>
2023-06-09 10:26:17 -0600

    Update 09jun2023n1.c

--- 09jun2023n1.c
+++ 09jun2023n1.c
@@ -13,11 +13,9 @@ int main()
     do{
         printf("\nel factorial n entre 2 y 20: ");
         scanf("%ld", &n);
-    } while((n<2) || (n>20));
+    } while ((n<2) || (n>20));
     
-    for (m=n, fact=1;n>1;fact*= n--)
-    {
-        printf("%ld! = %ld", m, fact);
-    }
+    for (m=n, fact=1;n>1; fact *= n--);
+    printf("%ld! = %ld", m, fact);
     return 0;
 }
