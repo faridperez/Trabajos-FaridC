10ce5971c1536785ef8de24e0542c8da7468cd9e
faridperez <131931890+faridperez@users.noreply.github.com>
2023-06-09 10:53:39 -0600

    Add files via upload

---
+++ 09jun2023n4.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/*
+farid yael perez de gabriel
+
+09/jun/2023
+
+numero1
+*/
+int main()
+{
+    int x;
+    do{
+        printf("%d ", x);
+        x++;
+    } while(x<=10);
+
+    return 0;
+}
